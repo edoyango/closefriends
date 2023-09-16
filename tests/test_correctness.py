3e97bdf897d9c9dec0c29c7cd00f2729dd399296
Edward <edward_yang_125@hotmail.com>
2023-09-16 21:00:53 +1000

    Merge duplicated code

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -1,19 +1,21 @@
-import ancillary, closefriends, scipy, numpy as np
+import ancillary, closefriends, scipy
 
-def test_1D():
-    x, cutoff, maxnpair = ancillary.setupf(1, 100, 4)
+def compare_ND(dim, maxPairsPerPoint):
+
+    x, cutoff, maxnpair = ancillary.setupf(dim, 100, maxPairsPerPoint)
 
     '''
-    output is (100, 2) ndarray. To compare, need to:
+    output is (100, dim) ndarray. To compare, need to:
         1. sort each pair so the lower index is first
         2. sort entire list by first index in each pair
     '''
+
     tree = scipy.spatial.cKDTree(x.transpose())
     pairs_scipy = tree.query_pairs(cutoff, output_type="ndarray")
     pairs_scipy = sorted([sorted(xi) for xi in pairs_scipy])
     
     '''
-    output is (1, 100) ndarray. To compare, need to:
+    output is (dim, 100) ndarray. To compare, need to:
         1. transpose for c-ordering
         2. sort each pair so the lower index is first
         3. sort entire list by first index in each pair
@@ -21,40 +23,16 @@ def test_1D():
     pairs_closefriends = closefriends.celllist(x, cutoff, maxnpair).transpose()
     pairs_closefriends = sorted([sorted(xi) for xi in pairs_closefriends])
 
-    assert pairs_closefriends == pairs_scipy
+    return pairs_closefriends == pairs_scipy
+
+def test_1D():
+    assert compare_ND(1, 4)
 
 def test_2D():
-    x, cutoff, maxnpair = ancillary.setupf(2, 100, 13)
-
-    tree = scipy.spatial.cKDTree(x.transpose())
-    pairs_scipy = tree.query_pairs(cutoff, output_type="ndarray")
-    pairs_scipy = sorted([sorted(xi) for xi in pairs_scipy])
-    
-    pairs_closefriends = closefriends.celllist(x, cutoff, maxnpair).transpose()
-    pairs_closefriends = sorted([sorted(xi) for xi in pairs_closefriends])
-    
-    assert pairs_closefriends == pairs_scipy
+    assert compare_ND(2, 13)
 
 def test_3D():
-    x, cutoff, maxnpair = ancillary.setupf(3, 100, 34)
-
-    tree = scipy.spatial.cKDTree(x.transpose())
-    pairs_scipy = tree.query_pairs(cutoff, output_type="ndarray")
-    pairs_scipy = sorted([sorted(xi) for xi in pairs_scipy])
-    
-    pairs_closefriends = closefriends.celllist(x, cutoff, maxnpair).transpose()
-    pairs_closefriends = sorted([sorted(xi) for xi in pairs_closefriends])
-    
-    assert pairs_closefriends == pairs_scipy
+    assert compare_ND(3, 34)
 
 def test_5D():
-    x, cutoff, maxnpair = ancillary.setupf(5, 100, 170)
-
-    tree = scipy.spatial.cKDTree(x.transpose())
-    pairs_scipy = tree.query_pairs(cutoff, output_type="ndarray")
-    pairs_scipy = sorted([sorted(xi) for xi in pairs_scipy])
-    
-    pairs_closefriends = closefriends.celllist(x, cutoff, maxnpair).transpose()
-    pairs_closefriends = sorted([sorted(xi) for xi in pairs_closefriends])
-    
-    assert pairs_closefriends == pairs_scipy
+    assert compare_ND(5, 166)
