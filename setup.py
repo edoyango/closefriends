9f0a12394fb749a62ec69b73f4254863249681ac
Edward <edward_yang_125@hotmail.com>
2023-09-16 22:31:30 +1000

    Expose only relevant functions

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from numpy.distutils.core import setup, Extension
 ext = Extension('closefriends.closefriends',
                        sources=['closefriends/src/closefriends.F90', 'closefriends/src/sort.cpp'],
                        libraries=['gfortran'],
-                       extra_compile_args=['-O3'])
+                       extra_compile_args=['-O3'],
+                       f2py_options=['only:', 'celllist_noreorder', 'celllist',':',])
 
 setup(
     name='closefriends',
