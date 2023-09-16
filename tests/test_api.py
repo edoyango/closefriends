5326ef1512c7ee42be79b2314f0adcadc5a55a70
Edward <edward_yang_125@hotmail.com>
2023-09-16 10:00:28 +1000

    Add return array shape test

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,3 +5,11 @@ def test_import():
         assert False, "Failed to import closefriends"
     else:
         assert True
+
+def test_returnarray():
+    import closefriends
+    from ancillary import setupf
+    x, cutoff, maxnpair = setupf(3, 100, 60)
+    pairs = closefriends.celllist(x, cutoff, maxnpair)
+    pairshape = pairs.shape
+    assert (len(pairshape)==2) & (pairshape[0]==2)
