2bd77d69f9c3c4f0f96f7b2100c22417b447e485
Edward <edward_yang_125@hotmail.com>
2023-09-17 20:47:19 +1000

    Adapt poetry for pybind11

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, Extension
 import pybind11
 
+ext_modules = [Extension(
+                        'closefriends',
+                        sources=['src/closefriends.cpp'],
+                        include_dirs=[pybind11.get_include()],
+                        language='c++',
+                        extra_compile_args=["-O3"]
+                       )]
+
 setup(
     name='closefriends',
     version='0.1',
-    ext_modules=[
-        Extension(
-            'closefriends',
-            sources=['src/closefriends.cpp'],
-            include_dirs=[pybind11.get_include()],
-            language='c++',
-            extra_compile_args=["-O3"]
-        ),
-    ],
+    ext_modules=ext_modules
 )
