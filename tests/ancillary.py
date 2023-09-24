ac81f8f4192e2fe267220ab2872f3704cde487c7
Edward <edward_yang_125@hotmail.com>
2023-09-24 17:51:41 +1000

    Use appropriate rng

--- tests/ancillary.py
+++ tests/ancillary.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 def setupc(dim, npoints, maxnpair_perp):
-    x = np.random.rand(npoints, dim)
+    rng = np.random.default_rng(12345)
+    x = rng.random(npoints*dim, dtype=np.float64).reshape((npoints, dim))
     cutoff = 2.0 * npoints ** (-1.0 / dim)
     maxnpair = maxnpair_perp * npoints
     return x, cutoff, maxnpair
